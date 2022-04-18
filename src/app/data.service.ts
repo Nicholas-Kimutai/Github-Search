e0dc08b5a91b304b0a891db4686df745e7975616
Nicholas-Kimutai <kimutainick74@gmail.com>
2022-04-19 00:49:19 +0300

    update data service

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+import { HttpClient } from '@angular/common/http';
+import { Users} from './users';
+import {Repo} from './repo';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DataService {
+  repos!: Repo;
+  users!:Users;
 
-  constructor() { }
+  constructor() {
+    this.users = new Users("",0,0,0,"","","");
+    this.repos = new Repo("","","","",new Date());
+   }
 }
