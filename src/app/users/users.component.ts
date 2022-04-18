32d808c7b3c2e410d2088b9606939155140c681c
Nicholas-Kimutai <kimutainick74@gmail.com>
2022-04-19 01:17:50 +0300

    add get profile function

--- src/app/users/users.component.ts
+++ src/app/users/users.component.ts
@@ -12,9 +12,25 @@ export class UsersComponent implements OnInit {
   user!:Users;
   repos:any;
 
-  constructor() { }
+  constructor(public DataService:DataService) { }
+  searchUser(username:string){
+    this.DataService.getProfile(username).then((success)=>{
+     this.user = this.DataService.users;
+    },
+    (error)=>{
+      console.log(error)
+    });
+    this.DataService.getRepo(username).then((success)=>{
+     this.repos = this.DataService.repos;
+    },
+    (error)=>{
+      console.log(error)
+    });
+  }
+
 
   ngOnInit(): void {
+    this.searchUser('Nicholas-Kimutai');
   }
 
 }
